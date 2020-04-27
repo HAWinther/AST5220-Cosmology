82ddf285a09316d0a8992d3e3517435c17d7f3f6
Hans Arnold Winther <hansw@tsih2.uio.no>
2020-04-27 14:35:09 +0200

    Fixed small bug for normal bessel func

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -164,7 +164,7 @@ namespace Utils{
   double J_n(const int n, const double x){
 #if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || (__cplusplus >= 201703L))
     // If you have a c++17 compiler you can use this
-    return std::cyl_bessel(n, x);
+    return std::cyl_bessel_j(n, x);
 #else
     if(n > 100 && x < 0.2 * n) return 0.0;
     return gsl_sf_bessel_Jn(n, x);
